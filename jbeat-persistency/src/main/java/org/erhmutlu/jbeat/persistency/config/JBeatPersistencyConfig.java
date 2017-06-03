48af7c5e0f75ccb8be7e2e905c945a3e1620b3f2
erhmutlu <erhmutlu@gmail.com>
2017-06-03 20:19:32 +0300

    jbeat-webapp module

---
+++ jbeat-persistency/src/main/java/org/erhmutlu/jbeat/persistency/config/JBeatPersistencyConfig.java
@@ -0,0 +1,11 @@
+package org.erhmutlu.jbeat.persistency.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by erhmutlu on 03/06/17.
+ */
+
+@Configuration
+public class JBeatPersistencyConfig {
+}
--- jbeat-persistency/src/main/java/org/erhmutlu/jbeat/persistency/converter/JpaJsonConverter.java
+++ jbeat-persistency/src/main/java/org/erhmutlu/jbeat/persistency/converter/JpaJsonConverter.java
@@ -2,6 +2,8 @@ package org.erhmutlu.jbeat.persistency.converter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.persistence.AttributeConverter;
 import java.io.IOException;
@@ -12,14 +14,18 @@ import java.util.Map;
  */
 public class JpaJsonConverter implements AttributeConverter<Map, String> {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     private final static ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public String convertToDatabaseColumn(Map map) {
         try {
             return objectMapper.writeValueAsString(map);
-        } catch (JsonProcessingException ex) {
-            return null;
+        } catch (Exception ex) {
+            //TODO can be better ?
+            logger.error(ex.getMessage());
+            throw new RuntimeException(ex);
         }
     }
 
@@ -27,8 +33,10 @@ public class JpaJsonConverter implements AttributeConverter<Map, String> {
     public Map convertToEntityAttribute(String dbData) {
         try {
             return (Map)objectMapper.readValue(dbData, Object.class);
-        } catch (IOException ex) {
-            return null;
+        } catch (Exception ex) {
+            //TODO can be better ?
+            logger.error(ex.getMessage());
+            throw new RuntimeException(ex);
         }
     }
 
