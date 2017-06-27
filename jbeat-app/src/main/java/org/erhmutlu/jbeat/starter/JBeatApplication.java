646061057fc536097fe63e3f66bd85b8652b651c
erhmutlu <erhmutlu@gmail.com>
2017-06-27 22:39:01 +0300

    #7 unnecessary codeis removed

--- jbeat-app/src/main/java/org/erhmutlu/jbeat/starter/JBeatApplication.java
+++ jbeat-app/src/main/java/org/erhmutlu/jbeat/starter/JBeatApplication.java
@@ -1,34 +1,10 @@
 package org.erhmutlu.jbeat.starter;
 
-import org.erhmutlu.jbeat.persistency.models.PeriodicTask;
-import org.erhmutlu.jbeat.service.PeriodicTaskService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class JBeatApplication {
-
-
-	@Bean
-	CommandLineRunner cmdRunner(PeriodicTaskService periodicTaskService){
-		return (strings -> {
-			try {
-				PeriodicTask periodicTask = periodicTaskService.getTaskById(251L);
-				System.out.println(periodicTask);
-				System.out.println("HİHİHİHİ");
-			}catch (Exception e){
-				System.out.println("LALALALALA");
-				e.printStackTrace();
-			}
-		});
-	}
-
-
-
 		public static void main(String[] args) {
 		SpringApplication.run(JBeatApplication.class, args);
 	}
